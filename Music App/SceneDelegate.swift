47b4662afea813932fb75c9356ba182c9de92cd8
vivianzhou1288 <87792254+vivianzhou1288@users.noreply.github.com>
2023-05-06 00:15:16 -0400

    Update SceneDelegate.swift

--- Music App/SceneDelegate.swift
+++ Music App/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  Music App
 //
-//  Created by Vivian Zhou on 5/5/23.
+//  Created by Vivian Zhou on 4/29/23.
 //
 
 import UIKit
@@ -16,7 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let viewController = ViewController()
+            window.rootViewController = UINavigationController(rootViewController: viewController)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
