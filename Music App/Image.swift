3764dc29f588ca4a68c0f3e95ac7a2177ab4311f
vivianzhou1288 <87792254+vivianzhou1288@users.noreply.github.com>
2023-05-06 00:19:04 -0400

    Create Image.swift

---
+++ Music App/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  Music App
+//
+//  Created by Vivian Zhou on 5/4/23.
+//
+
+import Foundation
+import UIKit
+
+struct Image: Codable {
+    var url: String
+    var created_at: String
+}
+
